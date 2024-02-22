51142abbbb997f447f2b645be50da6c6abbd1f50
josea <josea@Andreola-RGB>
2024-02-21 22:03:22 -0600

    Semana 6 Firebase

---
+++ Tienda/src/test/java/tienda_m_v2/TiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package tienda_m_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
