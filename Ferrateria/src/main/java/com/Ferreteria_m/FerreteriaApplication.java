a7fd6899d82c8039c6351daa093ecbba03b6f2b7
josea <josea@Andreola-RGB>
2024-03-11 14:03:30 -0600

    Proyecto base Ferreteria 1

---
+++ Ferrateria/src/main/java/com/Ferreteria_m/FerreteriaApplication.java
@@ -0,0 +1,13 @@
+package com.Ferreteria_m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerreteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FerreteriaApplication.class, args);
+	}
+
+}
