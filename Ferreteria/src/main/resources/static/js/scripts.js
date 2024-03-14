67fa2943d841aa0468e4c22510b0f6a5ba5205e0
josea <josea@Andreola-RGB>
2024-03-13 18:08:08 -0600

    Tabla creada SQL paginas info, categoria, producto e item1, item2

--- Ferreteria/src/main/resources/static/js/scripts.js
+++ Ferreteria/src/main/resources/static/js/scripts.js
@@ -5,3 +5,23 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+document.addEventListener("DOMContentLoaded", function() {
+    const cartButton = document.querySelector('.btn-outline-dark');
+    const cartPreview = document.getElementById('cartPreview');
+
+    cartButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        
+        // Aquí deberías mostrar la vista previa del carrito
+        cartPreview.innerHTML = '<h3>¡Vista previa del carrito!</h3>'; // Aquí puedes personalizar la vista previa como quieras
+        cartPreview.style.display = 'block'; // Mostrar la vista previa
+    });
+
+    // Evento para cerrar la vista previa al hacer clic en cualquier lugar fuera de la vista previa
+    document.addEventListener('click', function(event) {
+        if (event.target !== cartButton && !cartPreview.contains(event.target)) {
+            cartPreview.style.display = 'none'; // Ocultar la vista previa
+        }
+    });
+});
